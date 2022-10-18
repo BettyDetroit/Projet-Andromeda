e1be3b1e2eb0c0f988ac59f6893291c008987837
BettyDetroit <caubelnelly@gmail.com>
2022-10-18 17:16:35 +0200

    prompt works, not sure to keep it, final layout done, burger menu started, needs to be fixed tho

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const burger = document.querySelector('.burger');
+console.log(burger)
+const modal = document.querySelector('.modal');
+console.log('modal trouvé')
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    
+    burger.classList.toggle('active');  
+    modal.classList.toggle('active');
 })
+
+let nomViewer = document.querySelector('.insertName');
+console.log(nomViewer);
+
+// let userName = prompt("Salut Pionnier(e), quel est ton prénom?");
+// if(userName != undefined) {
+// nomViewer.innerText = `${userName}`;
+// }else{
+    // console.log("else");
+// nomViewer.innerText = `${""}`;
+// }
+
+
+
